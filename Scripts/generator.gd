de7539ac1d93d80ec69a5ce4f76cf36ab9ceacbc
Steven Guilbert <stevenguilbert24@gmail.com>
2022-10-12 19:51:37 -0500

    Fix rng generation

--- Scripts/generator.gd
+++ Scripts/generator.gd
@@ -3,19 +3,24 @@ extends Node
 
 var placeables_scene = preload("res://Scenes/Placeables/placeables.tscn")
 var placeables = []
+var rng = RandomNumberGenerator.new()
 
 func _ready():
+    rng.randomize()
+
     var instance = placeables_scene.instance()
     add_child(instance)
+    
     for placeable in instance.get_children():
         placeable.hide()
         placeables.append(placeable)
         
 
 func get_placeable():
-    var idx = randi() % placeables.size()
+    var idx = rng.randi() % placeables.size()
     var placeable = placeables[idx]
     var dup = placeable.duplicate()
-    dup.show()
-    return dup 
 
+    dup.show()
+
+    return dup 
