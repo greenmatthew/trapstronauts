6aa2a0747b28f46466543dcb1a3851114ea7bd9f
Steven Guilbert <stevenguilbert24@gmail.com>
2022-10-26 17:38:41 -0500

    Remove warnings

--- Assets/Sprites/Traps/BomberTrap.gd
+++ Assets/Sprites/Traps/BomberTrap.gd
@@ -5,26 +5,20 @@ var speed  = 65
 var motion = Vector2.ZERO
 var player = null
 
-func _physics_process(delta):
-	motion = Vector2.ZERO
-	if player:
-		motion = position.direction_to(player.position) * speed
-	motion = move_and_slide(motion)
-	for i in get_slide_count():
-		var collision = get_slide_collision(i)
-		if collision.collider.name == 'Player1':
-			print('Player got bombed')
-			
-			
-	
-	
-	
+func _physics_process(_delta):
+    motion = Vector2.ZERO
+    if player:
+        motion = position.direction_to(player.position) * speed
+    motion = move_and_slide(motion)
+    for i in get_slide_count():
+        var collision = get_slide_collision(i)
+        if collision.collider.name == 'Player1':
+            print('Player got bombed')
 
 func _on_Area2D_body_entered(body):
-	player = body
+    player = body
 
-
-func _on_Area2D_body_exited(body):
-	print('Player has exited')
-	player = null
-	
+func _on_Area2D_body_exited(_body):
+    print('Player has exited')
+    player = null
+    
