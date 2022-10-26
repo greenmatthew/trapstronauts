b31529d537bc34be70e84ee346635f59b527af10
unknown <jleeelle7@gmail.com>
2022-10-26 11:22:32 -0500

    Added DEBUG scene to be our dev playground

---
+++ Scripts/GameManager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+#var SPLASH, TITLE
+var MENU = 'res://Scenes/UI/MainMenu/MainMenu.tscn'
+#var CREDITS, SETTINGS
+#var HUB
+var FOREST = 'res://Scenes/Maps/map_forest.tscn'
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- Scripts/Maps/map_forest.gd
+++ Scripts/Maps/map_forest.gd
@@ -1,18 +1,6 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$MultiCam.add_target($Player1)
 	$MultiCam.add_target($TargetDummy)
 	print("Added")
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
