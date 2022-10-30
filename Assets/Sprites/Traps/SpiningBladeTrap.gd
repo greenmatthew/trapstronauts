a5474b2211863b52dbea5d976e2d13dc673cc08e
HimaneeshMukka <114118462+HimaneeshMukka@users.noreply.github.com>
2022-10-30 17:45:55 -0500

    Added SpinningBladeTrap

---
+++ Assets/Sprites/Traps/SpiningBladeTrap.gd
@@ -0,0 +1,17 @@
+extends Node2D
+export (int) var follow_speed = 50
+var player = null
+
+func _ready():
+	$AnimationPlayer.play("Spin")
+	
+func _process(delta):
+	$Path2D/PathFollow2D.offset += follow_speed * delta
+	
+	
+
+
+func _on_Area2D_body_entered(body):
+	if body is PlayerController:
+		var touched_player = get_tree().reload_current_scene()
+		print('Player got sliced')
