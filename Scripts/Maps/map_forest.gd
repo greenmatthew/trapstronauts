6b157c51bcb92f70b73df66e2c0f940b47c3942b
Matthew Green <greenmatthew07@gmail.com>
2022-11-07 18:41:51 -0600

    Finished general shape of forest map. Reorganized forest spritesheets.

--- Scripts/Maps/map_forest.gd
+++ Scripts/Maps/map_forest.gd
@@ -5,12 +5,10 @@ signal scene_changed(scene_from, scene_to)
 func add_player_to_world(player):
     add_child(player)
     var start = $Start
-    print("start pos: ", start.position)
     var spawn_points = $Start/SpawnPoints.get_children()
     if spawn_points != null and spawn_points.size() > 0:
         var spawn_point = spawn_points[randi() % spawn_points.size()] 
         player.position = start.position + spawn_point.position + Vector2.UP * 0.5 * player.get_collider_height()
-        print(player.global_position)
         player.rotation = spawn_point.rotation
     else:
         printerr("No spawn points found!")
