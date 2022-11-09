e8af9fbb82ef53991bd2f81fe4b132a6f2013baa
HimaneeshMukka <114118462+HimaneeshMukka@users.noreply.github.com>
2022-11-09 13:52:03 -0600

    Added Static Spike trap and Platform. Removed previous trap

--- Scripts/Traps/MovingPlatform.gd
+++ Scripts/Traps/MovingPlatform.gd
@@ -2,11 +2,7 @@ extends Node2D
 onready var timer = get_node("Timer")
 onready var timer2 = get_node("Timer2")
 
-func _ready():
-    $AnimationPlayer.play("movingplatform_flying")
 
-func _process(delta):
-    $movingplatform_path2d/movingplatform_follow2d.offset += 55 * delta
 
 
 
@@ -14,13 +10,13 @@ func _process(delta):
 
 func _on_Area2D_body_entered(body):
     if body is PlayerController:
-        timer.set_wait_time(2.7)
+        timer.set_wait_time(0.8)
         timer.start()
 
 
 func _on_Timer_timeout():
-    $movingplatform_path2d/movingplatform_follow2d/Sprite.visible=false
-    $movingplatform_path2d/movingplatform_follow2d/KinematicBody2D/CollisionShape2D.disabled= true
+    $KinematicBody2D/Sprite.visible = false
+    $KinematicBody2D/CollisionShape2D.disabled = true
     timer2.set_wait_time(1.5)
     timer2.start()
     timer.stop()
@@ -28,6 +24,6 @@ func _on_Timer_timeout():
 
 
 func _on_Timer2_timeout():
-    $movingplatform_path2d/movingplatform_follow2d/Sprite.visible=true
-    $movingplatform_path2d/movingplatform_follow2d/KinematicBody2D/CollisionShape2D.disabled= false
+    $KinematicBody2D/Sprite.visible = true
+    $KinematicBody2D/CollisionShape2D.disabled = false
     timer2.stop()
