3d0f67704034321d1a5732ebc7e351569aa28b84
Steven Guilbert <stevenguilbert24@gmail.com>
2022-11-09 19:31:04 -0600

    Add spikes to placeables

--- Scripts/Traps/SpikeDude.gd
+++ Scripts/Traps/SpikeDude.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-
-
-
-
 func _on_Area2D_body_entered(body):
     if body is PlayerController:
         EventHandler.emit_signal("player_killed", body, get_parent())
+
+func disable() -> void:
+    $Area2D/CollisionPolygon2D.set_deferred("disabled", true)
+
+func enable() -> void:
+    $Area2D/CollisionPolygon2D.set_deferred("disabled", false)
