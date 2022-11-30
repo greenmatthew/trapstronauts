dc21f9a62d89ac10874230ae9374ac7665a06969
HimaneeshMukka <mukka.himaneesh@gmail.com>
2022-11-29 20:22:55 -0600

    added sound to player walk

--- Scripts/StateMachine/PlayerStates/running.gd
+++ Scripts/StateMachine/PlayerStates/running.gd
@@ -4,10 +4,13 @@ func enter(_msg = {}) -> void:
     player.animator.play("run")
     player.walking_trail.emitting = false
     player.sprinting_trail.emitting = true
+    get_parent().get_parent().get_node("AudioStreamPlayer").play()
 
 func exit() -> void:
     player.walking_trail.emitting = false
     player.sprinting_trail.emitting = false
+    get_parent().get_parent().get_node("AudioStreamPlayer").stop()
+    
 
 func physics_update_extension(delta : float) -> void:
     if not player.is_on_floor():
