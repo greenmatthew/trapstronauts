200fb19d77ce558c48a4b0d79338e86b24a4a48f
HimaneeshMukka <mukka.himaneesh@gmail.com>
2022-11-29 20:42:31 -0600

    Added sound to jump

--- Scripts/StateMachine/PlayerStates/jumping.gd
+++ Scripts/StateMachine/PlayerStates/jumping.gd
@@ -6,9 +6,11 @@ func enter(_msg = {}):
     player.animator.play("jump")
     player.myjump(_msg.get("normal"))
     just_jumped = true
+    get_parent().get_parent().get_node("AudioStreamPlayer2").play()
 
 func physics_update_extension(delta : float) -> void:
     if not just_jumped:
+        get_parent().get_parent().get_node("AudioStreamPlayer").stop()
         if player.is_on_floor():
             state_machine.transition_to("Idle")
             return
