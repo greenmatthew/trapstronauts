b3e9c61bff82af6a91341567766d04b37ec7c3d2
Joshua Elledge <jelledgecg@gmail.com>
2022-11-30 18:11:01 -0600

    Added voting room skeleton, added portal

--- Scripts/Maps/DEBUG.gd
+++ Scripts/Maps/DEBUG.gd
@@ -4,25 +4,25 @@ onready var grid_manager = $GridManager
 var showing_selector = false
 
 func _ready():
-	$MultiCam.add_target($Player)
-	grid_manager.hide_selector_and_grid()
-	var _status = EventHandler.connect("player_killed", self, "_on_player_killed")
-	#$MultiCam.add_target($TargetDummy)
-	#print("Added")
+    $MultiCam.add_target($Player)
+    grid_manager.hide_selector_and_grid()
+    var _status = EventHandler.connect("player_killed", self, "_on_player_killed")
+    #$MultiCam.add_target($TargetDummy)
+    #print("Added")
 
 func _on_player_killed(player: PlayerController, trap: Placeable):
-	if (trap == null):
-		print(player.name, " killed by unknown")
-	else:
-		print(player.name, " killed by ", trap.name)
-	player.death()
+    if (trap == null):
+        print(player.name, " killed by unknown")
+    else:
+        print(player.name, " killed by ", trap.name)
+    player.death()
 
 func _unhandled_input(event):
-	if event is InputEventKey and event.scancode == KEY_TAB and event.pressed:
-		if showing_selector:
-			grid_manager.hide_selector_and_grid()
-			showing_selector = false
-		else:
-			grid_manager.show_selector_and_grid()
-			showing_selector = true
+    if event is InputEventKey and event.scancode == KEY_TAB and event.pressed:
+        if showing_selector:
+            grid_manager.hide_selector_and_grid()
+            showing_selector = false
+        else:
+            grid_manager.show_selector_and_grid()
+            showing_selector = true
 
