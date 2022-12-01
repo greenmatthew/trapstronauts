f3a7077256009fb88081ff5f7f63150dbc4dd6d4
Steven Guilbert <stevenguilbert24@gmail.com>
2022-12-01 05:26:18 -0600

    Add pause menu and pausing functionality. P to open.

--- Scenes/UI/Pause.gd
+++ Scenes/UI/Pause.gd
@@ -1,9 +1,15 @@
 extends Control
 
-signal resume
-
 func _on_Resume_pressed():
-    emit_signal("resume")
+    get_tree().paused = false
+    hide()
 
 func _on_Exit_pressed():
+    # EventHandler.emit_signal("scene_changed", "Pause", "MainMenu")
+    get_tree().paused = false
     var _status = get_tree().change_scene("res://Scenes/Main.tscn")
+
+func _unhandled_input(event):
+    if event is InputEventKey and event.scancode == KEY_P and event.is_pressed():
+        get_tree().paused = true
+        show()
