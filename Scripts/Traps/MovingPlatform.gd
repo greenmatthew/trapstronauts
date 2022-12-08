f674bb61e07b4e885cbdcd6a3405282f49a3771b
unknown <jleeelle7@gmail.com>
2022-12-08 12:03:57 -0600

    swinging trap art

--- Scripts/Traps/MovingPlatform.gd
+++ Scripts/Traps/MovingPlatform.gd
@@ -9,21 +9,21 @@ onready var timer2 = get_node("Timer2")
 
 
 func _on_Area2D_body_entered(body):
-	if body is PlayerController:
-		timer.set_wait_time(0.8)
-		timer.start()
+    if body is PlayerController:
+        timer.set_wait_time(0.8)
+        timer.start()
 
 
 func _on_Timer_timeout():
-	$KinematicBody2D/Sprite.visible = false
-	$KinematicBody2D/CollisionShape2D.disabled = true
-	timer2.set_wait_time(1.5)
-	timer2.start()
-	timer.stop()
-	
+    $KinematicBody2D/Sprite.visible = false
+    $KinematicBody2D/CollisionShape2D.disabled = true
+    timer2.set_wait_time(1.5)
+    timer2.start()
+    timer.stop()
+    
 
 
 func _on_Timer2_timeout():
-	$KinematicBody2D/Sprite.visible = true
-	$KinematicBody2D/CollisionShape2D.disabled = false
-	timer2.stop()
+    $KinematicBody2D/Sprite.visible = true
+    $KinematicBody2D/CollisionShape2D.disabled = false
+    timer2.stop()
